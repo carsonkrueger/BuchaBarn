67fdcdfb41e92a8dd394a920f5112820bd48df87
carsonkrueger <crkruege@gmail.com>
2022-11-06 23:25:49 -0700

    setting up navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./styles/navbar.css";
 
 export default () => {
-  <>
-    <Text>BuchaBarn</Text>
-  </>;
+  return (
+    <div id="nav">
+      <p id="bucha">BuchaBarn</p>
+      <p>Flavors</p>
+      <p>About</p>
+      <p>FAQ</p>
+    </div>
+  );
 };
