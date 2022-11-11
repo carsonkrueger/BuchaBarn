879cbd3f667f3c52cbbfbc0a5044132072c73e13
carsonkrueger <crkruege@gmail.com>
2022-11-10 19:13:11 -0700

    working on nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./styles/navbar.css";
+import logo1 from "./images/buchabarn4(1).png";
+import logo2 from "./images/buchabarn4.png";
 
 export default () => {
   return (
     <div id="nav">
-      <p id="bucha">BuchaBarn</p>
-      <p>Flavors</p>
-      <p>About</p>
-      <p>FAQ</p>
+      <img src={logo1} id="logo1" />
+
+      <div id="nav-text">
+        <p>Flavors</p>
+        <p>About</p>
+        <p>FAQ</p>
+        <p>Contact</p>
+      </div>
     </div>
   );
 };
