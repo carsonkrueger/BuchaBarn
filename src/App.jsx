dc64c3fea93fe763cc710613b296bcb27af5a67c
carsonkrueger <crkruege@gmail.com>
2022-11-11 23:43:27 -0700

    working on flavors

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import "./styles/app.css";
 
 import Navbar from "./Navbar";
 import Body from "./Body";
+import Flavors from "./Flavors";
 
 export default () => (
   <>
     <Navbar />
     <Body />
+    <Flavors />
   </>
 );
---
+++ src/Flavors.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/flavors.css";
+
+export default () => {
+  return (
+    <div class="flavors">
+      <p>Love at first sip!</p>
+    </div>
+  );
+};
